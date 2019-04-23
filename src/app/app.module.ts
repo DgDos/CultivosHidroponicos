26f6782c6517de0495a7d3676445ec0652e23863
DgDos <fijusohalo@gmail.com>
2019-04-23 14:34:27 -0500

    Packages Firebase & Material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import {Firebase} from 'src/app/firebase/firebase';
+import {AngularFireModule} from '@angular/fire';
+import { AngularFirestore } from '@angular/fire/firestore';
+
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SensoresComponent } from './sensores/sensores.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SensoresComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(Firebase.firebase)
   ],
-  providers: [],
+  providers: [AngularFirestore],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
