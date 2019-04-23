1ab9a141ea8c8bc9d088db8352e71ea9e271105b
anfegave98 <anfegave98@hotmail.com>
2019-04-23 17:10:41 -0500

    Card of plants full

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
 import {Firebase} from 'src/app/firebase/firebase';
 import {AngularFireModule} from '@angular/fire';
 import { AngularFirestore } from '@angular/fire/firestore';
@@ -15,9 +16,10 @@ import { SensoresComponent } from './sensores/sensores.component';
 import {MatCardModule} from '@angular/material/card';
 import {MatButtonModule} from '@angular/material/button';
 import {MatCheckboxModule} from '@angular/material/checkbox';
-
-
-
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatRadioModule} from '@angular/material/radio';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatListModule} from '@angular/material/list';
 
 @NgModule({
   declarations: [
@@ -31,7 +33,11 @@ import {MatCheckboxModule} from '@angular/material/checkbox';
     AngularFireModule.initializeApp(Firebase.firebase),
     MatCardModule,
     MatButtonModule,
-    MatCheckboxModule
+    MatCheckboxModule,
+    MatGridListModule,
+    MatRadioModule,
+    MatDividerModule,
+    MatListModule
   ],
   providers: [AngularFirestore],
   bootstrap: [AppComponent]
