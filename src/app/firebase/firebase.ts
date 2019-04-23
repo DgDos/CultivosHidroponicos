5d563e3892c7d42f2f1d105dbfab3e389fd5207c
DgDos <fijusohalo@gmail.com>
2019-04-23 12:45:06 -0500

    firebase class

--- src/app/firebase/firebase.ts
+++ src/app/firebase/firebase.ts
@@ -1,2 +1,11 @@
-export class Firebase {
-}
+export const Firebase = {
+    production: false,
+    firebase: {
+        apiKey: "AIzaSyB683WpW1x4IGeVZxrMns5dNMLWrxPunR0",
+        authDomain: "cultivoshidroponicos-8e6a3.firebaseapp.com",
+        databaseURL: "https://cultivoshidroponicos-8e6a3.firebaseio.com",
+        projectId: "cultivoshidroponicos-8e6a3",
+        storageBucket: "cultivoshidroponicos-8e6a3.appspot.com",
+        messagingSenderId: "801506431978"
+    }
+};
